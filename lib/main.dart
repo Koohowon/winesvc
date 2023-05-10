d5726a4c4e888bac77bae1df1926c21db4e81ba7
Android Studio Fullter <qkrrn0315@gmail.com>
2023-05-10 13:31:00 +0900

    상단 앱바 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+//콤마 들어가는거는 전부다 파라미터
 void main() {
   runApp(const MyApp());
 }
@@ -13,18 +13,45 @@ class MyApp extends StatelessWidget {
       home: Scaffold( //상중하로 나눠주는 위젯
         appBar: AppBar(
           centerTitle: true, // 타이틀을 중간으로 한다
-          title: Text('와인 추천 서비스 APP'),
-
-          actions: [ //오른쪽 끝에
+          title: Text('와인 추천 서비스 APP',
+            style: TextStyle(
+              color: Colors.black, //텍스트 색상 변경
+            ),),
+          backgroundColor: Colors.grey, // 앱바 배경 색상
+          leading: IconButton( //왼쪽 끝에
+            icon: Icon(Icons.menu),//메뉴 아이콘 생성
+            color: Colors.black, //아이콘 색상 변경
+            onPressed: (){
+            },
+          ),
+          actions: <Widget>[ //오른쪽 끝에
             IconButton(  //아이콘 생성
-              icon: const Icon(Icons.call), //전화모양 아이콘 생성
+              icon: Icon(Icons.call), //전화모양 아이콘 생성
+              color: Colors.black, //아이콘 색상 변경
               onPressed: () {// 버튼이 눌렸을 때 동작할 코드
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context){
+                    return AlertDialog(
+                      title: Text("고객센터"),
+                      content: Text("TEL : 010 - 7387 - 7262"),
+                    );
+                  },
+                );
               },
             ),
           ],
         ), //상단에 들어갈 위젯
-        body: Container(), //중단에 들어갈 위젯
-        bottomNavigationBar: BottomAppBar(), //하단에 들어갈 위젯
+        body: InkWell( // 이 위젯으로 컨테이너 위젯 감싸서 그 위젯 클릭하게 만듬 (사전적 의미?)약간 위젯에 제스처 기능을 추가하는거?
+          onTap: () {
+
+          },
+
+        ), //중단에 들어갈 위젯
+        bottomNavigationBar: BottomAppBar(
+
+
+        ), //하단에 들어갈 위젯
       ),
       debugShowCheckedModeBanner: false, // 오른쪽 위에 리본 없애기
     );
