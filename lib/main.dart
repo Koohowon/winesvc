025c33ea9c11a93a524fe7122318fec5cd90bbea
Android Studio Fullter <qkrrn0315@gmail.com>
2023-05-10 14:47:00 +0900

    하단 상호와 설명란 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,12 @@ class MyApp extends StatelessWidget {
             style: TextStyle(
               color: Colors.black, //텍스트 색상 변경
             ),),
-          backgroundColor: Colors.grey, // 앱바 배경 색상
+          backgroundColor: Colors.white, // 앱바 배경 색상
           leading: IconButton( //왼쪽 끝에
             icon: Icon(Icons.menu),//메뉴 아이콘 생성
             color: Colors.black, //아이콘 색상 변경
             onPressed: (){
+
             },
           ),
           actions: <Widget>[ //오른쪽 끝에
@@ -49,8 +50,21 @@ class MyApp extends StatelessWidget {
 
         ), //중단에 들어갈 위젯
         bottomNavigationBar: BottomAppBar(
-
-
+          child: Column(
+            mainAxisSize: MainAxisSize.min, //알아서 최소한의 사이즈로 맞춰주는 위젯
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('(주)주류주류', // 상호명 따로 만들고 밑에 사이즈박스랑 별개
+                  style: TextStyle(fontSize: 15, //폰트 사이즈
+                  fontWeight: FontWeight.bold)), //폰트 볼드체
+              SizedBox(height: 5,), //여기에 설명란처럼 만들기,위에 텍스랑의 간격 차이
+              Text("대표 : 구호원",style: TextStyle(fontSize: 12)),
+              Text("사업자 번호 : 211-00-12345",style: TextStyle(fontSize: 12)),
+              Text("본사 : 부산 광역시 해운대구 ",style: TextStyle(fontSize: 12)),
+              Text("운영시간 : 10:00 ~ 18:00 주말/공휴일 휴무",style: TextStyle(fontSize: 12)),
+              Text("와인 서비스 추천 애플리케이션이며 판매는 하지 않고 있습니다. ",style: TextStyle(fontSize: 12)),
+            ],
+          ),
         ), //하단에 들어갈 위젯
       ),
       debugShowCheckedModeBanner: false, // 오른쪽 위에 리본 없애기
